cef3b4373e8689d644e387312551c0b65f96caa1
jason.hua <jason.hua@PC-JASONHUA>
2019-07-16 18:19:20 +0800

    位运算:二进制中1的个数

---
+++ CodingInterviews/CodingInterviews/Coding011.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingInterviews
+{
+    /// <summary>
+    /// 位运算:二进制中1的个数
+    /// 输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示。
+    /// </summary>
+    public class Coding011
+    {
+        /// <summary>
+        /// 针对正整数(负数不会走)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int SimpleNumberOf1(int n) {
+            int count = 0;
+            while (n > 0) { //右移的处理，
+                if ((n & 1) == 1) {
+                    count++;
+                }
+                n = n >> 1;
+            }
+            return count;
+        }
+
+        /// <summary>
+        /// 新颖解法：
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static int NumberOf1(int n) {
+            int count = 0;
+            while (n != 0) {
+                count++;
+                n = (n - 1) & n;
+            }
+            return count;
+        }
+
+
+    }
+}
---
+++ CodingInterviews/CodingTest/Coding011Test.cs
@@ -0,0 +1,62 @@
+﻿using CodingInterviews;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CodingTest
+{
+    public class Coding011Test
+    {
+        [Fact]
+        public void Test1()
+        {
+            int data = 1;//0001
+            Assert.Equal(1, Coding011.SimpleNumberOf1(data));
+            Assert.Equal(1, Coding011.NumberOf1(data));
+            data = 0b0001;
+            Assert.Equal(1, Coding011.SimpleNumberOf1(data));
+            Assert.Equal(1, Coding011.NumberOf1(data));
+
+            data = 0b0100;
+            Assert.Equal(1, Coding011.SimpleNumberOf1(data));
+            Assert.Equal(1, Coding011.NumberOf1(data));
+            data = 4;//0100
+            Assert.Equal(1, Coding011.SimpleNumberOf1(data));
+            Assert.Equal(1, Coding011.NumberOf1(data));
+
+            data = 0b1000;
+            Assert.Equal(1, Coding011.SimpleNumberOf1(data));
+            Assert.Equal(1, Coding011.NumberOf1(data));
+            data = 8;//1000
+            Assert.Equal(1, Coding011.SimpleNumberOf1(data));
+            Assert.Equal(1, Coding011.NumberOf1(data));
+
+            data = 0;
+            Assert.Equal(0, Coding011.SimpleNumberOf1(data));
+            Assert.Equal(0, Coding011.NumberOf1(data));
+
+            data = int.MaxValue;
+            Assert.Equal(31, Coding011.SimpleNumberOf1(data));
+            Assert.Equal(31, Coding011.NumberOf1(data));
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var data = -1;
+            Assert.Equal(32, Coding011.NumberOf1(data));
+
+            data = -4;
+            Assert.Equal(30, Coding011.NumberOf1(data));
+
+            data = -8;
+            Assert.Equal(29, Coding011.NumberOf1(data));
+
+            data = int.MinValue;
+            Assert.Equal(1, Coding011.NumberOf1(data));
+        }
+
+
+    }
+}
