5d2711771853e4f12bb5cdac6d64dbef2a1a03b6
jason.hua <jason.hua@PC-JASONHUA>
2019-07-18 16:36:38 +0800

    代码的完整性:调整数组顺序使奇数位于偶数前面【临时提交】

---
+++ CodingInterviews/CodingInterviews/Coding013.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingInterviews
+{
+    /// <summary>
+    /// 代码的完整性:调整数组顺序使奇数位于偶数前面
+    /// 输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有的奇数位于数组的前半部分，
+    /// 所有的偶数位于数组的后半部分，并保证奇数和奇数，偶数和偶数之间的相对位置不变。
+    /// </summary>
+    public class Coding013
+    {
+        public static int[] HandleArray(int[] array) {
+            if (array == null) return null;
+            int[] temp = new int[array.Length];
+            int i = 0;
+            //奇数
+            foreach (var t in array) {
+                if (t % 2 == 1) {
+                    temp[i] = t;
+                    i++;
+                }
+            }
+            //偶数
+            foreach (var t in array)
+            {
+                if (t % 2 == 0)
+                {
+                    temp[i] = t;
+                    i++;
+                }
+            }
+            return temp;
+        }
+
+        /// <summary>
+        /// 不考略相对位置的
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static int[] HandleArray2(int[] array)
+        {
+            if (array == null) return null;
+
+            int left = 0;
+            int right = array.Length - 1;
+            while (left < right)
+            {
+                //奇数
+                while (array[left] % 2 == 1)
+                {
+                    left++;
+                }
+
+                //偶数
+                while (left < right && array[right] % 2 == 0)
+                {
+                    right--;
+                }
+
+                //交换
+                if (left < right)
+                {
+                    var temp = array[left];
+                    array[left] = array[right];
+                    array[right] = temp;
+                }
+
+            }
+            return array;
+        }
+
+        /// <summary>
+        /// 考略相对位置的
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static int[] HandleArray3(int[] array)
+        {
+            if (array == null) return null;
+
+            //上一次交换的位置
+            int preLeft = 0;
+            int preRight = 0;
+
+            int left = 0;
+            int right = array.Length - 1;
+
+            while (left < right)
+            {
+                //奇数
+                while (array[left] % 2 == 1)
+                {
+                    left++;
+                }
+
+                //偶数
+                while (left < right && array[right] % 2 == 0)
+                {
+                    right--;
+                }
+
+                if (left < right)
+                {
+                    var temp = array[left];
+                    array[left] = array[right];
+                    array[right] = temp;
+
+                    if (preLeft != preRight)
+                    {
+                        //数据依次后移
+                        var tempLeftData = array[preLeft];
+                        array[preLeft] = array[left];
+                        array[left] = tempLeftData;
+
+                        var tempRightData = array[preRight];
+                        array[preRight] = array[right];
+                        array[right] = tempRightData;
+                    }
+
+                    preLeft = left;
+                    preRight = right;
+                }
+
+            }
+            return array;
+        }
+
+
+    }
+}
---
+++ CodingInterviews/CodingTest/Coding013Test.cs
@@ -0,0 +1,60 @@
+﻿using CodingInterviews;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CodingTest
+{
+    public class Coding013Test
+    {
+        [Fact]
+        public void Test1()
+        {
+            int[] array = { 1, 2, 3, 4, 5 };
+            int[] result = { 1, 3, 5, 2, 4 };
+            Assert.Equal(JsonConvert.SerializeObject(result), JsonConvert.SerializeObject(Coding013.HandleArray(array)));
+        }
+
+        [Fact]
+        public void Test0()
+        {
+            int[] array = { 0 };
+            int[] result = { 0 };
+            Assert.Equal(JsonConvert.SerializeObject(result), JsonConvert.SerializeObject(Coding013.HandleArray(array)));
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            int[] array = { 1 };
+            int[] result = { 1 };
+            Assert.Equal(JsonConvert.SerializeObject(result), JsonConvert.SerializeObject(Coding013.HandleArray(array)));
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            int[] array = { 2 };
+            int[] result = { 2 };
+            Assert.Equal(JsonConvert.SerializeObject(result), JsonConvert.SerializeObject(Coding013.HandleArray(array)));
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            int[] array = { 1, 3, 5 };
+            int[] result = { 1, 3, 5 };
+            Assert.Equal(JsonConvert.SerializeObject(result), JsonConvert.SerializeObject(Coding013.HandleArray(array)));
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            int[] array = { 2, 4, 6 };
+            int[] result = { 2, 4, 6 };
+            Assert.Equal(JsonConvert.SerializeObject(result), JsonConvert.SerializeObject(Coding013.HandleArray(array)));
+        }
+    }
+}
