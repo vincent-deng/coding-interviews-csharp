7dcaa57e5c3df2e7b64eac89b65353410143c7a4
jason.hua <jason.hua@PC-JASONHUA>
2019-07-22 17:28:00 +0800

    代码的鲁棒性:树的子结构

---
+++ CodingInterviews/CodingInterviews/Coding017.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingInterviews
+{
+    /// <summary>
+    /// 代码的鲁棒性:树的子结构
+    /// 输入两棵二叉树A，B，判断B是不是A的子结构。（ps：我们约定空树不是任意一个树的子结构）
+    /// 
+    /// 解题思路：
+    /// 查找B是不是A的子结构，其实就是查找B是不是A的一部分
+    /// 1.先找出A和B一样的根节点R
+    /// 2.判断R的左右节点和B是否一样
+    /// 每次都是根节点，左右节点，递归实现
+    /// 递归的终结是如果之前的节点均相同，最后子树为空时，而父树如果也是NULL，则说明两颗树完全一样，如果父树不是NULL，则子树是父树的一部分
+    /// </summary>
+    public class Coding017
+    {
+        /// <summary>
+        /// check 左右子树是否相同
+        /// </summary>
+        /// <param name="parent"></param>
+        /// <param name="child"></param>
+        /// <returns></returns>
+        private static bool CheckLeftAndRightNode(TreeNode parent, TreeNode child)
+        {
+            if (child == null) return true; //子树为NULL，那么必然是子树
+            if (parent == null) return false; //子树不是空，父树为空
+
+            if (child.val != parent.val)
+            {
+                return false;
+            }
+            else {
+                return CheckLeftAndRightNode(parent.left, child.left) & CheckLeftAndRightNode(parent.right, child.right);
+            }
+        }
+
+        /// <summary>
+        /// 查找子树
+        /// </summary>
+        /// <param name="parent"></param>
+        /// <param name="child"></param>
+        /// <returns></returns>
+        public static bool HasSubTree(TreeNode parent, TreeNode child)
+        {
+            if (parent == null || child == null) return false;
+            bool result = false;
+            if (parent.val == child.val)
+            {
+                result = CheckLeftAndRightNode(parent, child);
+            }
+
+            if (!result)
+            {
+                result = HasSubTree(parent.left, child) || HasSubTree(parent.right, child);
+            }
+            return result;
+        }
+    }
+}
---
+++ CodingInterviews/CodingTest/Coding017Test.cs
@@ -0,0 +1,138 @@
+﻿using CodingInterviews;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace CodingTest
+{
+    public class Coding017Test
+    {
+        /// <summary>
+        /// 普通二叉树
+        ///              1                    1
+        ///           /     \              /     \
+        ///          2       3            2       3  
+        ///         /       / \          /       / \
+        ///        4       5   6        4       5   6
+        ///         \         /          \         /
+        ///          7       8            7       8   
+        /// </summary>
+        [Fact]
+        public void Test1()
+        {
+            int[] preTree = { 1, 2, 4, 7, 3, 5, 6, 8 };//前序
+            int[] midTree = { 4, 7, 2, 1, 5, 3, 8, 6 };//中序
+            TreeNode tree = Coding004.Tree(preTree.ToList(), midTree.ToList());
+
+            int[] preTree2 = { 1, 2, 4, 7, 3, 5, 6, 8 };
+            int[] midTree2 = { 4, 7, 2, 1, 5, 3, 8, 6 };
+            TreeNode child = Coding004.Tree(preTree2.ToList(), midTree2.ToList());
+
+            Assert.True(Coding017.HasSubTree(tree, child));
+
+            int[] preTree3 = { 1, 2, 4, 7, 3, 5, 9, 8 };
+            int[] midTree3 = { 4, 7, 2, 1, 5, 3, 8, 9 };
+            TreeNode child3 = Coding004.Tree(preTree3.ToList(), midTree3.ToList());
+            Assert.False(Coding017.HasSubTree(tree, child3));
+        }
+
+        /// <summary>
+        /// 普通二叉树
+        ///              1                    1
+        ///           /     \              /  
+        ///          2       3            2   
+        ///         /       / \          /    
+        ///        4       5   6        4    
+        ///         \         /          \      
+        ///          7       8            7     
+        /// </summary>
+        [Fact]
+        public void Test2()
+        {
+            int[] preTree = { 1, 2, 4, 7, 3, 5, 6, 8 };
+            int[] midTree = { 4, 7, 2, 1, 5, 3, 8, 6 };
+            TreeNode tree = Coding004.Tree(preTree.ToList(), midTree.ToList());
+
+            int[] preTree2 = { 1, 2, 4, 7};
+            int[] midTree2 = { 4, 7, 2, 1};
+            TreeNode child = Coding004.Tree(preTree2.ToList(), midTree2.ToList());
+            Assert.True(Coding017.HasSubTree(tree, child));
+        }
+
+        /// <summary>
+        /// 普通二叉树
+        ///              1                
+        ///           /     \             
+        ///          2       3            2   
+        ///         /       / \          /    
+        ///        4       5   6        4    
+        ///         \         /          \      
+        ///          7       8            7     
+        /// </summary>
+        [Fact]
+        public void Test3()
+        {
+            int[] preTree = { 1, 2, 4, 7, 3, 5, 6, 8 };
+            int[] midTree = { 4, 7, 2, 1, 5, 3, 8, 6 };
+            TreeNode tree = Coding004.Tree(preTree.ToList(), midTree.ToList());
+
+            int[] preTree2 = {  2, 4, 7 };
+            int[] midTree2 = { 4, 7, 2 };
+            TreeNode child = Coding004.Tree(preTree2.ToList(), midTree2.ToList());
+            Assert.True(Coding017.HasSubTree(tree, child));
+
+        }
+
+
+        /// <summary>
+        /// 所有结点都没有右子结点
+        ///            1     1
+        ///           /     /
+        ///          2     2 
+        ///         / 
+        ///        3 
+        /// </summary>
+        [Fact]
+        public void Right()
+        {
+            int[] preTree = { 1, 2, 3 };
+            int[] midTree = { 3, 2, 1 };
+            TreeNode tree = Coding004.Tree(preTree.ToList(), midTree.ToList());
+
+            int[] preTree2 = { 1, 2, 3 };
+            int[] midTree2 = { 2, 1 };
+            TreeNode child = Coding004.Tree(preTree2.ToList(), midTree2.ToList());
+            Assert.True(Coding017.HasSubTree(tree, child));
+        }
+
+        /// <summary>
+        /// 所有结点都没有左子结点
+        ///            1     
+        ///             \ 
+        ///              2      2
+        ///               \      \
+        ///                3      3
+        ///                 \      
+        ///                  4
+        ///                   \
+        ///                    5
+        /// </summary>
+        [Fact]
+        public void Left()
+        {
+            int[] preTree = { 1, 2, 3, 4, 5 };
+            int[] midTree = { 1, 2, 3, 4, 5 };
+            TreeNode tree = Coding004.Tree(preTree.ToList(), midTree.ToList());
+
+            int[] preTree2 = { 2, 3 };
+            int[] midTree2 = { 2, 3 };
+            TreeNode child = Coding004.Tree(preTree2.ToList(), midTree2.ToList());
+            Assert.True(Coding017.HasSubTree(tree, child));
+
+        }
+
+    }
+}
