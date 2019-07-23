89620f9ee91ded1ef3b1a2b8c2117b80326da998
jason.hua <jason.hua@PC-JASONHUA>
2019-07-23 13:58:11 +0800

    包含min函数的栈

---
+++ CodingInterviews/CodingInterviews/Coding020.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingInterviews
+{
+    /// <summary>
+    /// 举例让抽象具体化:包含min函数的栈
+    /// 定义栈的数据结构，请在该类型中实现一个能够得到栈中所含最小元素的min函数（时间复杂度应为O（1））。
+    /// 
+    /// 解题思路：维持两个栈，一个保存数据，一个保存最小值
+    /// </summary>
+    public class Coding020
+    {
+
+    }
+
+    /// <summary>
+    /// min函数栈
+    /// </summary>
+    public class MinInStack
+    {
+        public Stack<int> dataStack;
+        public Stack<int> minStack;
+
+        public MinInStack()
+        {
+            dataStack = new Stack<int>();
+            minStack = new Stack<int>();
+        }
+
+        public int Pop()
+        {
+            if (dataStack.Count == 0) return -1;
+            minStack.Pop();
+            return dataStack.Pop();
+        }
+
+        public void Push(int item)
+        {
+            dataStack.Push(item);
+            if (!minStack.TryPeek(out int data) || data > item) //每次放进去的都是最小值
+            {
+                minStack.Push(item);
+            }
+            else
+            {
+                minStack.Push(data);
+            }
+        }
+
+        public int Min()
+        {
+            if (dataStack.Count == 0) return -1;
+
+            return minStack.Peek();
+        }
+    }
+}
---
+++ CodingInterviews/CodingTest/Coding020Test.cs
@@ -0,0 +1,82 @@
+﻿
+using CodingInterviews;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace CodingTest
+{
+    public class Coding020Test
+    {
+        [Fact]
+        public void Test() {
+            MinInStack stack = new MinInStack();
+            stack.Push(1);
+            Assert.Equal(1, stack.Min());
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            MinInStack stack = new MinInStack();
+            stack.Push(1);
+            stack.Push(2);
+            Assert.Equal(1, stack.Min());
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            MinInStack stack = new MinInStack();
+            stack.Push(3);
+            stack.Push(2);
+            stack.Push(1);
+            Assert.Equal(1, stack.Min());
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            MinInStack stack = new MinInStack();
+            stack.Push(3);
+            stack.Push(2);
+            stack.Push(1);
+            Assert.Equal(1, stack.Min());
+            stack.Pop();
+            Assert.Equal(2, stack.Min());
+            stack.Pop();
+            Assert.Equal(3, stack.Min());
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            MinInStack stack = new MinInStack();
+            stack.Push(3);
+            stack.Push(2);
+            stack.Push(2);
+            stack.Push(4);
+            Assert.Equal(2, stack.Min());
+            stack.Pop();
+            Assert.Equal(2, stack.Min());
+            stack.Pop();
+            stack.Pop();
+            Assert.Equal(3, stack.Min());
+        }
+
+        [Fact]
+        public void Test6()
+        {
+            MinInStack stack = new MinInStack();
+            stack.Push(3);
+            stack.Push(2);
+            stack.Push(2);
+            stack.Push(4);
+            Assert.Equal(2, stack.Min());
+            stack.Pop();
+            stack.Push(1);
+            Assert.Equal(1, stack.Min());
+        }
+    }
+}
