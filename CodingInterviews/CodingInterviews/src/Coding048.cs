a05048cac915b71cadc10ba319514e1e7180f89e
jason.hua <jason.hua@PC-JASONHUA>
2019-07-26 15:54:21 +0800

    其他程序源码

---
+++ CodingInterviews/CodingInterviews/src/Coding048.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingInterviews
+{
+    public class Coding048
+    {
+        public static int SumRecursion(int n)
+        {
+            Console.WriteLine(n);
+            int sum = n;
+            //当n>0为假的时候，右边将不执行
+            bool ans = (n > 0) && ((sum += SumRecursion(n - 1)) > 0);
+            //或者下面，短路与（&&）和短路或（||）
+            //bool ans = (n <= 0) || ((sum += SumRecursion(n - 1)) > 0);
+            return sum;
+        }
+    }
+}
---
+++ CodingInterviews/CodingInterviews/src/Coding052.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingInterviews
+{
+    class Coding052
+    {
+        public static int[] multiply(int[] data)
+        {
+            int length = data.Length;
+            int[] res = new int[length];
+            // C[i] = A[0] * A[1] * ... *A[i - 1]=1*2*3*4*5
+            for (int i = 0, temp = 1; i < length; i++)
+            {
+                res[i] = temp;
+                temp *= data[i];
+            }
+
+            // D[i] = A[i + 1] * A[i + 2] * ... *A[n - 1]=
+            for (int i = length - 1, temp = 1; i >= 0; i--)
+            {
+                res[i] *= temp;
+                temp *= data[i];
+
+            }
+            return res;
+        }
+    }
+}
+
